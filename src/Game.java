bdaa4310a9b9ad44a7daeb4162656e5a696881a3
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-09-24 11:08:04 +0100

    Create Game.java

---
+++ src/Game.java
@@ -0,0 +1,35 @@
+public class Game {
+    private int id;
+    private String name;
+    private String category;
+
+    public Game(int id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
