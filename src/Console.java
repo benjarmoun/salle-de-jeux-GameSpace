36887b48be93adbd990b83edfe96311c6ec7cfda
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2022-09-24 11:07:30 +0100

    console class first commit

---
+++ src/Console.java
@@ -0,0 +1,35 @@
+public class Console {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Console(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
